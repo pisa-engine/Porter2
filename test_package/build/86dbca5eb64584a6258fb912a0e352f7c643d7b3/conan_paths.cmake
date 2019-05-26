ac4f2021cf34638595313a82acaab2d56b24535d
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2019-05-25 21:22:45 -0400

    Remove build dir

--- test_package/build/86dbca5eb64584a6258fb912a0e352f7c643d7b3/conan_paths.cmake
+++
@@ -1,2 +0,0 @@
-set(CMAKE_MODULE_PATH "/home/elshize/.conan/data/Porter2/1.0/pisa-engine/testing/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/" ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
-set(CMAKE_PREFIX_PATH "/home/elshize/.conan/data/Porter2/1.0/pisa-engine/testing/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/" ${CMAKE_PREFIX_PATH} ${CMAKE_CURRENT_LIST_DIR})
